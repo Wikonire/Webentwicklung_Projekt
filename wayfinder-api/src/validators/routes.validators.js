6d20d0b8f6b0666fe0f87be032f8edfc9871ad93
Wikonire <ni.widmer@bluewin.ch>
2025-08-28 11:10:16 +0200

    fix(validators): Add TODO comment
    Included a TODO comment to clarify the expected return values of true/false in the `validateCreateRoute` function. This serves as a reminder for future updates to enhance code readability.

--- wayfinder-api/src/validators/routes.validators.js
+++ wayfinder-api/src/validators/routes.validators.js
@@ -1,6 +1,6 @@
 const isLon = (x) => Number.isFinite(x) && x >= -180 && x <= 180;
 const isLat = (x) => Number.isFinite(x) && x >= -90 && x <= 90;
-
+// TODO Verständlich schreiben, Rückgabewerte true, false
 function validateCreateRoute(b) {
     const req = ['userId','startLat','startLng','endLat','endLng','geometry'];
     for (const k of req) if (b[k] === undefined) return `${k} fehlt`;
