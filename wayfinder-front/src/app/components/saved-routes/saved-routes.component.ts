4f3efa67d6e2e8b2f838e88ac9aeb0a8b90f759e
Wikonire <ni.widmer@bluewin.ch>
2025-09-11 22:05:21 +0200

    feat(front): Add profile icons and map display functionality to routes
    Introduced profile icons and labels to saved routes for better UI clarity. Added functionality to display a selected route on the map using an EventEmitter. Updated template to reflect these changes and improve the user experience.

--- wayfinder-front/src/app/components/saved-routes/saved-routes.component.ts
+++ wayfinder-front/src/app/components/saved-routes/saved-routes.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import {Component, EventEmitter, inject, Output} from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouteStore } from '../../services/route-store.service';
-import {SavedRoute} from '../../models/route-result.model';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {AppRoute} from '../../models/routes.model';
+import {OrsProfile} from '../../models/ors-profile.model';
+import {profiles} from '../../models/ors-profile.model';
 
 @Component({
   selector: 'app-saved-routes',
@@ -15,14 +17,27 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 })
 export class SavedRoutesComponent {
   private routeStore = inject(RouteStore);
+  @Output() showRoute = new EventEmitter();
 
+  /** Icon zum Profil finden */
+  getProfileIcon(profile: OrsProfile): string {
+    return profiles.find(p => p.value === profile)?.icon ?? 'help_outline';
+  }
+
+  getProfileLabel(profile: OrsProfile): string {
+    return profiles.find(p => p.value === profile)?.label ?? 'help_outline';
+  }
   savedRoutes = this.routeStore.savedRoutes;
 
-  displayRoute(route: SavedRoute): string {
+  displayRoute(route: AppRoute): string {
     return `${route.name}`;
   }
 
   remove(id: string): void {
     this.routeStore.removeSavedRoute(id);
+  }
+
+  toMap(id: string): void {
+    this.showRoute.emit(id);
   }
 }
