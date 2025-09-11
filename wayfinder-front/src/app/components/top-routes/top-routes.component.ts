ff5595fe4b8579415ebc4904f2a8d756488dde99
Wikonire <ni.widmer@bluewin.ch>
2025-09-11 22:04:40 +0200

    feat(front): Add profile icons and tooltips to top routes list
    Enhanced UI by displaying profile-specific icons and tooltips in the top routes list. This improves clarity by visually distinguishing route profiles and providing descriptive tooltips for better user understanding. Added corresponding styles to align icons with the text.

--- wayfinder-front/src/app/components/top-routes/top-routes.component.ts
+++ wayfinder-front/src/app/components/top-routes/top-routes.component.ts
@@ -1,20 +1,32 @@
 import {Component, Input, inject} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
-import {TopSearchEntry, RouteStore} from '../../services/route-store.service';
+import {RouteStore} from '../../services/route-store.service';
 import {MatIconModule} from '@angular/material/icon';
+import {TopSearchEntry} from '../../models/routes.model';
+import {OrsProfile, profiles} from '../../models/ors-profile.model';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-top-routes',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, MatIconModule],
+  imports: [MatListModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './top-routes.component.html',
   styleUrls: ['./top-routes.component.scss']
 })
 export class TopRoutesComponent {
   @Input() topRoutes: TopSearchEntry[] = [];
 
+  /** Icon zum Profil finden */
+  getProfileIcon(profile: OrsProfile): string {
+    return profiles.find(p => p.value === profile)?.icon ?? 'help_outline';
+  }
+
+  getProfileLabel(profile: OrsProfile): string {
+    return profiles.find(p => p.value === profile)?.label ?? 'help_outline';
+  }
   private routeStore = inject(RouteStore);
+  profiles = profiles;
 
   save(entry: TopSearchEntry): void {
     this.routeStore.saveFromTopSearch(entry);
