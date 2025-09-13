cf54580b0c7f7391da437560bf1b8a44f9d98682
Wikonire <ni.widmer@bluewin.ch>
2025-09-14 00:03:14 +0200

    feat(front): Refactor saved routes to use inputs and helper service.
    Replaced internal savedRoutes management with an @Input() for handling routes externally. Integrated DisplayRouteService to streamline route display logic and updated emit logic to include profiles. Adjusted template bindings to reflect these changes.

--- wayfinder-front/src/app/components/saved-routes/saved-routes.component.ts
+++ wayfinder-front/src/app/components/saved-routes/saved-routes.component.ts
@@ -1,23 +1,28 @@
-import {Component, EventEmitter, inject, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouteStore } from '../../services/route-store.service';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {AppRoute} from '../../models/routes.model';
-import {OrsProfile} from '../../models/ors-profile.model';
-import {profiles} from '../../models/ors-profile.model';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AppRoute } from '../../models/routes.model';
+import { OrsProfile, profiles } from '../../models/ors-profile.model';
+import { CommonModule } from '@angular/common';
+import {RouteStore} from '../../services/route-store.service';
+import {DisplayRouteService} from '../../services/display-route.service';
 
 @Component({
   selector: 'app-saved-routes',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [CommonModule, MatListModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './saved-routes.component.html',
   styleUrls: ['./saved-routes.component.scss']
 })
 export class SavedRoutesComponent {
   private routeStore = inject(RouteStore);
-  @Output() showRoute = new EventEmitter();
+  displayRouteHelper = inject(DisplayRouteService);
+  @Output() showRoute = new EventEmitter<{ id:string, profile:OrsProfile }>();
+
+  /** Liste der gespeicherten Routen vom Backend */
+  @Input() savedRoutes: AppRoute[] = [];
 
   /** Icon zum Profil finden */
   getProfileIcon(profile: OrsProfile): string {
@@ -27,17 +32,13 @@ export class SavedRoutesComponent {
   getProfileLabel(profile: OrsProfile): string {
     return profiles.find(p => p.value === profile)?.label ?? 'help_outline';
   }
-  savedRoutes = this.routeStore.savedRoutes;
 
-  displayRoute(route: AppRoute): string {
-    return `${route.name}`;
-  }
 
   remove(id: string): void {
     this.routeStore.removeSavedRoute(id);
   }
 
-  toMap(id: string): void {
-    this.showRoute.emit(id);
+  toMap(id: string, profile:OrsProfile): void {
+    this.showRoute.emit({id, profile});
   }
 }
